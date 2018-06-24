7efd5f1a6e31504094e71d9008cbdcd38986b4ac
N3vix <argentiym@gmail.com>
2018-06-24 19:01:15 +0300

    +Prototype, Builder

---
+++ Patterns/Builder/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baker baker = new Baker();
+
+            BreadBuilder builder = new RyeBreadBuilder();
+
+            Bread ryeBread = baker.Bake(builder);
+            Console.WriteLine(ryeBread.ToString());
+
+            builder = new WheatBreadBuilder();
+
+            Bread wheatBread = baker.Bake(builder);
+            Console.WriteLine(wheatBread.ToString());
+
+            Console.Read();
+        }
+    }
+    
+
+
+    class Baker
+    {
+        public Bread Bake(BreadBuilder breadBuilder)
+        {
+            breadBuilder.CreateBread();
+            breadBuilder.SetFlour();
+            breadBuilder.SetSalt();
+            breadBuilder.SetAdditives();
+            return breadBuilder.Bread;
+        }
+    }
+
+    abstract class BreadBuilder
+    {
+        public Bread Bread { get; private set; }
+        public void CreateBread()
+        {
+            Bread = new Bread();
+        }
+        public abstract void SetFlour();
+        public abstract void SetSalt();
+        public abstract void SetAdditives();
+    }
+    class RyeBreadBuilder : BreadBuilder
+    {
+        public override void SetFlour()
+        {
+            this.Bread.Flour = new Flour { Sort = "Flour 1" };
+        }
+
+        public override void SetSalt()
+        {
+            this.Bread.Salt = new Salt();
+        }
+
+        public override void SetAdditives()
+        {
+            // null
+        }
+    }
+    class WheatBreadBuilder : BreadBuilder
+    {
+        public override void SetFlour()
+        {
+            this.Bread.Flour = new Flour { Sort = "Flour 2" };
+        }
+
+        public override void SetSalt()
+        {
+            this.Bread.Salt = new Salt();
+        }
+
+        public override void SetAdditives()
+        {
+            this.Bread.Additives = new Additives { Name = "additives" };
+        }
+    }
+
+    class Flour
+    {
+        public string Sort { get; set; }
+    }
+    class Salt
+    { }
+    class Additives
+    {
+        public string Name { get; set; }
+    }
+    class Bread
+    {
+        public Flour Flour { get; set; }
+        public Salt Salt { get; set; }
+        public Additives Additives { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (Flour != null)
+                sb.Append(Flour.Sort + "\n");
+            if (Salt != null)
+                sb.Append("Salt \n");
+            if (Additives != null)
+                sb.Append("Additives: " + Additives.Name + " \n");
+            return sb.ToString();
+        }
+    }
+}
